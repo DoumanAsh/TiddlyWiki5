f63f3e9654fe0cf14284496f86e51fd60a14a8bb
DoumanAsh <douman@gmx.se>
2023-11-12 14:00:50 +0900

    Prepare wrapper for AES crypto

--- core/modules/utils/crypto.js
+++ core/modules/utils/crypto.js
@@ -12,6 +12,111 @@ Utility functions related to crypto.
 /*global $tw: false */
 "use strict";
 
+/**
+ * Creates Crypto instance with optional `salt` to derive key components
+ *
+ * If not used use some pre-defined constant
+ */
+function Crypto(salt) {
+    //node.js provides compatibility with Web API
+    var inner = typeof window !== 'undefined' ? window.crypto : require('crypto').webcrypto;
+    var enc = new TextEncoder();
+    var dec = new TextEncoder();
+
+    if (typeof salt === 'string') {
+        salt = enc.encode(salt)
+    } else if (typeof salt === 'undefined') {
+        salt = Uint8Array.from([84, 105, 100, 100, 108, 121, 87, 105, 107, 105, 53])
+    }
+
+    function randomLen(length) {
+        return inner.getRandomValues(new Uint8Array(length));
+    }
+
+    function importKey(password) {
+        return inner.subtle.importKey(
+            "raw",
+            enc.encode(password),
+            "PBKDF2",
+            false,
+            ["deriveBits", "deriveKey"],
+        );
+    }
+    function deriveKey(keyMaterial) {
+        var params = {
+            name: "PBKDF2",
+            salt,
+            iterations: 100000,
+            hash: "SHA-256",
+        };
+        return inner.subtle.deriveKey(
+            params,
+            keyMaterial,
+            { name: "AES-GCM", length: 256 },
+            true,
+            ["encrypt", "decrypt"],
+        );
+    }
+
+    function generateKey(password) {
+        return importKey(password).then(deriveKey)
+    }
+
+    function encrypt(input, key_or_password) {
+        var key = typeof key_or_password === 'CryptoKey' ? Promise.resolve(key_or_password) : this.generateKey(password);
+        var algo = {
+            name: "AES-GCM",
+            iv: randomLen(96),
+        };
+        function perform_encrypt(key) {
+            return inner.subtle.encrypt(algo, key, enc.encode(input))
+        }
+
+        function encode(buffer) {
+            var codec = new TextDecoder('latin1');
+            return JSON.stringify({
+                //todo base64?
+                data: codec.decode(buffer),
+                iv: codec.decode(algo.iv)
+            });
+        }
+
+        return key.then(perform_encrypt).then(encode);
+    }
+
+    function decrypt(input, key_or_password) {
+        var codec = new TextEncoder('latin1');
+        var key = typeof key_or_password === 'CryptoKey' ? Promise.resolve(key_or_password) : this.generateKey(password);
+
+        function perform_decrypt(key)  {
+            input = JSON.parse(input);
+            var data = codec.encode(input.data);
+            var iv = codec.encode(input.iv);
+            var algo = {
+                name: "AES-GCM",
+                iv
+            };
+
+            return inner.subtle.decrypt(algo, key, data);
+        }
+
+        function decode(buffer) {
+            return dec.decode(buffer);
+        }
+
+        key.then(perform_decrypt).then(decode)
+    }
+
+    return {
+        ///Async function to generate Key for AES-GCM algo
+        generateKey,
+        ///Async function to perform AES encryption
+        encrypt,
+        ///Async function to perform AES decryption
+        decrypt,
+    };
+}
+
 /*
 Look for an encrypted store area in the text of a TiddlyWiki file
 */
